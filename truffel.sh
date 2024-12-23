a4984a6d96fcac6fbd21e2540483d860f7bd9245
Ambuj kumar <33841424+GuardianByte@users.noreply.github.com>
2024-12-23 21:06:27 +0100

    Update truffel.sh

--- truffel.sh
+++ truffel.sh
@@ -1,17 +1,11 @@
-# Define Slack webhook URL
 SLACK_WEBHOOK_URL="Enter slack webhook"
 
-# Function to send a message to Slack webhook
 send_slack_notification() {
     curl -X POST -H 'Content-type: application/json' --data '{"text":"'"$1"'"}' $SLACK_WEBHOOK_URL
 }
 
-# Notify start of scan
 send_slack_notification "Trufflehog scan started."
 
 for i in $(cat gitrepo.txt) ; do docker run --rm -it -v "$PWD:/pwd" trufflesecurity/trufflehog:latest github --repo https://github.com/organisation_name/$i.git --only-verified --exclude-detectors=GCP,SlackWebhook,FacebookOAuth,Twilio --token=token-value > /home/ubuntu/truffelhog_automation/truffel_output/git-$i.txt ; done
 
-# Notify completion of scan
 send_slack_notification "Trufflehog scan completed."
-
-#This script is compatable on ubuntu server
